940c2a0f0c98cc32a402bbe636705505e3a368fb
timon <nomit007@gmail.com>
2012-12-05 23:05:48 +0700

    Added Register AutoMapper profile task

---
+++ src/MvcExtensions.AutoMapper.Tests/RegistrationProfileBaseTests.cs
@@ -0,0 +1,27 @@
+﻿namespace MvcExtensions.AutoMapper.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Xunit;
+
+    public class RegistrationProfileBaseTests
+    {
+        
+        private sealed class DefaultProfileRegistration : MvcExtensions.AutoMapper.RegisterProfileBase
+        {
+            protected override IEnumerable<Assembly> GetAutoMapperProfileAssemblyList()
+            {
+                return null;
+            }
+        }
+
+
+        [Fact]
+        public void Should_be_throw_argument_exception()
+        {
+            DefaultProfileRegistration defaultProfileRegistration = new DefaultProfileRegistration();
+            Assert.Throws<ArgumentNullException>(() => { defaultProfileRegistration.Execute(); });
+        }
+    }
+}
--- src/MvcExtensions.AutoMapper/Properties/AssemblyInfo.cs
+++ src/MvcExtensions.AutoMapper/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvsExtensions.AutoMapper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MvcExtensions.AutoMapper")]
+[assembly: AssemblyDescription("B")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MvsExtensions.AutoMapper")]
+[assembly: AssemblyProduct("MvcExtensions.AutoMapper")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
