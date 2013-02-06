9fe3ba0bb7f01e405a254e528f47a651bd0779a3
timon <nomit007@gmail.com>
2013-02-06 21:42:34 +0700

    bug fix

---
+++ src/MvcExtensions.AutoMapper/RegisterProfileBase.cs
@@ -0,0 +1,53 @@
+﻿namespace MvcExtensions.AutoMapper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using MvcExtensions;
+    using System.Linq;
+    using global::AutoMapper;
+
+    public abstract class RegisterProfileBase : BootstrapperTask
+    {
+        /// <summary>
+        /// Execute the task; 
+        /// </summary>
+        /// <returns></returns>
+        public override TaskContinuation Execute()
+        {
+            Registration();
+
+            return TaskContinuation.Continue;
+        }
+
+        /// <summary>
+        /// Registration Profile subclass
+        /// </summary>
+        protected void Registration()
+        {
+            foreach (var profile in GetProfiles())
+                Mapper.AddProfile(profile);
+        }
+
+        /// <summary>
+        /// Return all profiles on scaning assembles
+        /// </summary>
+        /// <returns></returns>
+        protected IEnumerable<Profile> GetProfiles()
+        {
+            var assemblies = GetAutoMapperProfileAssemblyList();
+            Invariant.IsNotNull(assemblies, "assemblies");
+
+            return assemblies.SelectMany(assembly => assembly.GetTypes()
+                                                             .Where(c => c.IsSubclassOf(typeof(Profile)))
+                                                             .Select(Activator.CreateInstance)).Cast<Profile>();
+        }
+
+        /// <summary>
+        /// Return assembles in which there is the automapper profiles
+        /// </summary>
+        /// <returns></returns>
+        protected abstract IEnumerable<Assembly> GetAutoMapperProfileAssemblyList();
+
+    }
+}
